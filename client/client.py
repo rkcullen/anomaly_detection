f4619a84a4eed9574ee0e45becaf6b1d4a326d42
Robert Cullen <cinquaterra@gmail.com>
2021-06-27 19:36:45 -0400

    Refactored the directory, added Dockerfiles

--- client/client.py
+++ client/client.py
@@ -2,7 +2,9 @@
 from __future__ import print_function
 from datetime import datetime
 import logging
+import sys
 
+import os
 import pandas as pd
 import grpc
 import csv
@@ -11,16 +13,22 @@ import json
 import anomaly_detection_pb2
 import anomaly_detection_pb2_grpc
 
-def run():
 
-    with grpc.insecure_channel("localhost:50051") as channel:
+def run():
+    
+    anomaly_detection_host = os.getenv("ANOMALY_DETECTION_HOST", "localhost")
+    with grpc.insecure_channel(f"{anomaly_detection_host}:50051") as channel:
         stub = anomaly_detection_pb2_grpc.TwitterAnomalyDetectionStub(channel)
         with open('test_data_1.csv', "r") as f:
             data1 = f.read() + "\n"
             result = stub.DetectAnomalies(anomaly_detection_pb2.Request(raw_json=data1, only_last="day"))
         for key in sorted (result.pairs):
-            print(key + ": " + str(result.pairs[key]))
+            root.info(key + ": " + str(result.pairs[key]))
 
 if __name__ == '__main__':
-    logging.basicConfig()
+    logging.basicConfig(filename='output.log', level=logging.INFO)
+    root = logging.getLogger()
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    root.addHandler(handler)
     run()
--- server.Dockerfile
+++ server.Dockerfile
@@ -1,13 +1,13 @@
 FROM python:3
 
 RUN mkdir /service
-COPY protobufs/ /service/
-COPY client/ /service/
-WORKDIR /service
+COPY . /service/
+WORKDIR /service/
 RUN python -m pip install --upgrade pip
 RUN python -m pip install grpcio-tools
-RUN python -m pip install pandas 
+RUN python -m pip install tad
 RUN python -m grpc_tools.protoc -I . --python_out=. \
 	   --grpc_python_out=. anomaly_detection.proto
 
-ENTRYPOINT [ "python", "client.py" ]
+EXPOSE 50051
+ENTRYPOINT [ "python", "server.py" ]
